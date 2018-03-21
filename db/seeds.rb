d65184f934524f88d5a05a54e635b9a7ecc79fd1
Kolby <kolbykalafut@gmail.com>
2018-03-21 16:37:04 -0400

    Checkpoint 5 seeding: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  user = User.new
+  pw = FFaker::Internet.password
+
+  user.email = FFaker::Internet.email
+  user.password = pw
+  user.password_confirmation = pw
+
+  user.skip_confirmation!
+  user.save!
+end
+
+# Create my test user
+  user = User.new
+  pw = "password"
+
+  user.email = "kolbykalafut@gmail.com"
+  user.password = pw
+  user.password_confirmation = pw
+
+  user.skip_confirmation!
+  user.save!
+
+  users = User.all
+
+50.times do
+ item = Item.new
+ item.name = FFaker::Lorem.sentence
+ item.user = users.sample
+
+ item.save!
+end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -19,21 +19,21 @@ RSpec.describe ItemsController, type: :controller do
 
     describe "POST #create" do
       it "increases the number of items by 1" do
-        expect { post :create, params: { user_id: user.id, item: { name: FFaker::Name.name } } }.to change(Item, :count).by(1)
+        expect { post :create, params: { user_id: user.id, item: { name: FFaker::Lorem.sentence } } }.to change(Item, :count).by(1)
       end
 
       it "assigns the new item to @item" do
-        post :create, params: { user_id: user.id, item: { name: FFaker::Name.name } }
+        post :create, params: { user_id: user.id, item: { name: FFaker::Lorem.sentence } }
         expect(assigns(:item)).to eq Item.last
       end
 
       it "associates the logged in user with the new item" do
-        post :create, params: { user_id: user.id, item: { name: FFaker::Name.name } }
+        post :create, params: { user_id: user.id, item: { name: FFaker::Lorem.sentence } }
         expect(Item.last.user_id).to eq user.id
       end
 
       it "redirects to the users show view" do
-        post :create, params: { user_id: user.id, item: { name: FFaker::Name.name } }
+        post :create, params: { user_id: user.id, item: { name: FFaker::Lorem.sentence } }
         expect(response).to redirect_to users_show_path
       end
     end
